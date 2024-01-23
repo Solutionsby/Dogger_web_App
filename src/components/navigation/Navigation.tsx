d7a18064cc8982d5e83b0f323f5d9ead053d942e
Pawel Goc <pawel@Mac-mini-Pawel.local>
2024-01-23 21:53:29 +0100

    v1

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -5,8 +5,9 @@ import { Link } from 'react-router-dom'
 export const Navigation =()=>{
     return(
         <div className="navigation-wrapper">
-            <a href="https://www.sejsza.pl/"><div className="logo"></div></a>
+            <a href="https://www.sejsza.pl/" className='logo-link'><div className="logo"></div></a>
             <Link to="/adoption">Adopcja</Link>
+            <Link to="/">Nasi Podobpieczni</Link>
         </div>
     )
 }
